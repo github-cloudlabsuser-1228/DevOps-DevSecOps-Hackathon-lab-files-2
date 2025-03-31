7e0393eb1bc9d8b790ca9b8a13fc74fa422a1e75
github_cloudlabsuser_1228@cloudlabsaiuser.com <github_cloudlabsuser_1228@cloudlabsaiuser.com>
2025-03-31 11:29:47 +0000

    first commit

---
+++ ContosoTraders.Ui.Website.V2.Raw/ContosoTraders.Ui.Website.V2/pages/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoTraders.Ui.Website.V2.Pages.Public
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Any logic for the About Us page can go here.
+        }
+    }
+}
